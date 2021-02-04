0073ee23e683a7636716e0a72ee2faff418c279c
mikeletux <miguel.sama@dell.com>
2021-02-04 15:29:57 +0100

    VirtualMedia resource started

--- redfish/resource_redfish_virtual_media.go
+++ redfish/resource_redfish_virtual_media.go
@@ -2,9 +2,12 @@ package redfish
 
 import (
 	"context"
+	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/stmcginnis/gofish"
+	"github.com/stmcginnis/gofish/redfish"
 	//"log"
 )
 
@@ -51,37 +54,42 @@ func getResourceRedfishVirtualMediaSchema() map[string]*schema.Schema {
 			},
 		},
 		"virtual_media_id": {
-			Type:        schema.TypeList,
+			Type:        schema.TypeString,
 			Description: "ID from the virtual media to be used. I.E: RemovableDisk",
 			Required:    true,
 		},
 		"image": {
-			Type:        schema.TypeList,
+			Type:        schema.TypeString,
+			Description: "The URI of the remote media to attach to the virtual media",
+			Required:    true,
+		},
+		"inserted": {
+			Type:        schema.TypeBool,
 			Description: "The URI of the remote media to attach to the virtual media",
 			Required:    true,
 		},
 		"username": {
-			Type:        schema.TypeList,
+			Type:        schema.TypeString,
 			Description: "The username to access the image parameter-specific URI",
 			Optional:    true,
 		},
 		"password": {
-			Type:        schema.TypeList,
+			Type:        schema.TypeString,
 			Description: "The password to access the image parameter-specific URI",
 			Optional:    true,
 		},
 		"transfer_method": {
-			Type:        schema.TypeList,
+			Type:        schema.TypeString,
 			Description: "",
 			Optional:    true,
 		},
 		"transfer_protocol_type": {
-			Type:        schema.TypeList,
+			Type:        schema.TypeString,
 			Description: "",
 			Optional:    true,
 		},
 		"write_protected": {
-			Type:        schema.TypeList,
+			Type:        schema.TypeBool,
 			Description: "",
 			Optional:    true,
 		},
@@ -125,6 +133,44 @@ func resourceVirtualMediaDelete(ctx context.Context, d *schema.ResourceData, m i
 
 func createRedfishVirtualMedia(service *gofish.Service, d *schema.ResourceData) diag.Diagnostics {
 	var diags diag.Diagnostics
+
+	//Get terraform schema data
+	virtualMediaID := d.Get("virtual_media_id").(string)
+	image := d.Get("image").(string)
+
+	var username string
+	if v, ok := d.GetOk("username"); ok {
+		username = v.(string)
+	}
+	var password string
+	if v, ok := d.GetOk("password"); ok {
+		password = v.(string)
+	}
+	var transferMethod string
+	if v, ok := d.GetOk("transfer_method"); ok {
+		transferMethod = v.(string)
+	}
+	var transferProtocolType string
+	if v, ok := d.GetOk("transfer_protocol_type"); ok {
+		transferProtocolType = v.(string)
+	}
+
+	//Get OOB Manager card - managers[0] will be our oob card
+	managers, err := service.Managers()
+	if err != nil {
+		return diag.Errorf("Couldn't retrieve managers from redfish API: %s", err)
+	}
+
+	virtualMediaCollection, err := managers[0].VirtualMedia()
+	if err != nil {
+		return diag.Errorf("Couldn't retrieve virtual media collection from redfish API: %s", err)
+	}
+	//Get specific virtual media
+	virtualMedia, err := getVirtualMedia(virtualMediaID, virtualMediaCollection)
+	if err != nil {
+		return diag.Errorf("%s", err)
+	}
+
 	return diags
 }
 
@@ -141,4 +187,13 @@ func updateRedfishVirtualMedia(ctx context.Context, service *gofish.Service, d *
 func deleteRedfishVirtualMedia(service *gofish.Service, d *schema.ResourceData) diag.Diagnostics {
 	var diags diag.Diagnostics
 	return diags
+}
+
+func getVirtualMedia(virtualMediaID string, vms []*redfish.VirtualMedia) (*redfish.VirtualMedia, error) {
+	for _, v := range vms {
+		if v.ID == virtualMediaID {
+			return v, nil
+		}
+	}
+	return nil, fmt.Errorf("VirtualMedia with ID %s doesn't exist", virtualMediaID)
 }
