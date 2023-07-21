cb1d5e86adb2939058e6a32cd0d02878094044ad
Sakshi_makkar <Sakshi_makkar@dell.com>
2023-07-21 07:41:22 -0400

    Added test case for bios data source test

---
+++ redfish/data_source_redfish_bios_test.go
@@ -0,0 +1,41 @@
+package redfish
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"os"
+	"testing"
+)
+
+// redfish.Power represents a concrete Go type that represents an API resource
+func TestAccRedfishBiosDataSource_basic(t *testing.T) {
+	os.Setenv("TF_ACC", "1")
+
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRedfishDataSourceBiosConfig(creds),
+			},
+		},
+	})
+}
+
+func testAccRedfishDataSourceBiosConfig(testingInfo TestingServerCredentials) string {
+	return fmt.Sprintf(`
+		
+		data "redfish_bios" "bios" {
+		
+		  redfish_server {
+			user = "%s"
+			password = "%s"
+			endpoint = "https://%s"
+			ssl_insecure = true
+		  }
+		}
+		`,
+		testingInfo.Username,
+		testingInfo.Password,
+		testingInfo.Endpoint,
+	)
+}
