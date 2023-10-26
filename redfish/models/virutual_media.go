2c5b8e76ba1d29c2ebb22a8f6ef7df29b8416541
prajwalpatil25 <prajwal.patil@dell.com>
2023-10-26 00:44:36 -0400

    removed comments

--- redfish/models/virutual_media.go
+++ redfish/models/virutual_media.go
@@ -13,32 +13,3 @@ type VirtualMedia struct {
 	TransferProtocolType types.String  `tfsdk:"transfer_protocol_type"`
 	WriteProtected       types.Bool    `tfsdk:"write_protected"`
 }
-
-// type TransferMethodType struct {
-// 	Stream types.String `tfsdk:"stream"`
-// 	Upload types.String `tfsdk:"upload"`
-// }
-
-// type TransferProtocolType struct {
-// 	CIFSTransferProtocolType  types.String `tfsdk:"CIFS"`
-// 	FTPTransferProtocolType   types.String `tfsdk:"FTP"`
-// 	SFTPTransferProtocolType  types.String `tfsdk:"SFTP"`
-// 	HTTPTransferProtocolType  types.String `tfsdk:"HTTP"`
-// 	HTTPSTransferProtocolType types.String `tfsdk:"HTTPS"`
-// 	NFSTransferProtocolType   types.String `tfsdk:"NFS"`
-// 	SCPTransferProtocolType   types.String `tfsdk:"SCP"`
-// 	TFTPTransferProtocolType  types.String `tfsdk:"TFTP"`
-// 	OEMTransferProtocolType   types.String `tfsdk:"OEM"`
-// }
-
-// TransferMethod is how the data is transferred.
-// type TransferMethod string
-
-// const (
-
-// 	// StreamTransferMethod Stream image file data from the source URI.
-// 	StreamTransferMethod TransferMethod = "Stream"
-// 	// UploadTransferMethod Upload the entire image file from the source URI
-// 	// to the service.
-// 	UploadTransferMethod TransferMethod = "Upload"
-// )
