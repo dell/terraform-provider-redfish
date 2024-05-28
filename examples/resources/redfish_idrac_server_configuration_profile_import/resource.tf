66540947563bf6c9287c63235a7b3a90b49c99cf
Priyanshu Krishnan <priyanshu.krishnan@dell.com>
2024-05-28 02:10:35 -0400

    update copyrights

--- examples/resources/redfish_idrac_server_configuration_profile_import/resource.tf
+++ examples/resources/redfish_idrac_server_configuration_profile_import/resource.tf
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021-2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+Copyright (c) 2024 Dell Inc., or its subsidiaries. All Rights Reserved.
 
 Licensed under the Mozilla Public License Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
