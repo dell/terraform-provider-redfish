323344a3e6c408e1f3457a091ce5619e6797e9e0
Priyanshu Krishnan <118425422+Krishnan-Priyanshu@users.noreply.github.com>
2024-07-17 10:49:39 +0530

    added system_id to boot datasource and deprecated resource_id (#222)
    * added system_id to boot datasource and deprecated resource_id
    
    * fix gen issue

--- redfish/models/system_boot.go
+++ redfish/models/system_boot.go
@@ -26,6 +26,7 @@ type SystemBootDataSource struct {
 	RedfishServer                []RedfishServer `tfsdk:"redfish_server"`
 	ID                           types.String    `tfsdk:"id"`
 	ResourceID                   types.String    `tfsdk:"resource_id"`
+	SystemID                     types.String    `tfsdk:"system_id"`
 	BootOrder                    types.List      `tfsdk:"boot_order"`
 	BootSourceOverrideEnabled    types.String    `tfsdk:"boot_source_override_enabled"`
 	BootSourceOverrideMode       types.String    `tfsdk:"boot_source_override_mode"`
