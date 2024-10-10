2e80be980a2cc022ac3467fcc7fea1ae17e9a4a5
Tao He <tao.he1@dell.com>
2024-10-10 17:10:00 +0800

    draft4

--- main.go
+++ main.go
@@ -30,12 +30,11 @@ import (
 
 // If you do not have terraform installed, you can remove the formatting command, but its suggested to
 // ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
 
 // Run the docs generation tool, check its repository for more information on how it works and how docs
 // can be customized.
 //nolint:all
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@v0.19.4 generate --provider-name terraform-provider-redfish
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@v0.19.4 generate --rendered-website-dir docs --provider-name terraform-provider-redfish
 
 func main() {
 	var debug bool
